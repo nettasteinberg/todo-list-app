3d40bf3ceba125f572b81c4358cc687ec5ef1c13
Netta Steinberg <netta.steinberg@gmail.com>
2023-04-13 21:58:11 +0300

    'remove' button and 'done' button work inside li

--- main.js
+++ main.js
@@ -7,17 +7,32 @@ const taskList = document.querySelector(".taskList");
 addButton.addEventListener("click", function(event) {
     if (inputField.value != "") {
         const li = document.createElement("li");
-        li.innerText = inputField.value;        
         
+        //add the text inside a p element
+        const p = document.createElement("p");
+        p.innerText = inputField.value;        
+        li.append(p);
+
+        const buttonsDiv = document.createElement("div");
+        buttonsDiv.classList.add("buttonsDiv");
+
         //add a "remove" button to the li element
         const removeButton = document.createElement("button");
         removeButton.innerText = "Remove";
         removeButton.addEventListener("click", function(event) {
             taskList.removeChild(li);
         })
-        li.append(removeButton);
+        buttonsDiv.append(removeButton);
 
         //add a "done" button to the li element
+        const doneButton = document.createElement("button");
+        doneButton.innerText = "Done";
+        doneButton.addEventListener("click", function(event) {
+            p.style.textDecoration = "line-through";
+        })
+        buttonsDiv.append(doneButton);
+
+        li.append(buttonsDiv);
 
         taskList.append(li);
         inputField.value = "";
